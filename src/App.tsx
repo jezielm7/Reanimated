396b5abced11c1ce84a71086a7eccae8b69c7b41
Jeziel Marques <jezielmarques7@gmail.com>
2020-09-30 19:50:03 -0300

    Reanimated

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Drag from './pages/Drag';
+import Login from './pages/Login';
+import Scroll from './pages/Scroll';
+import JellyScroll from './pages/JellyScroll';
+
+const App: React.FC = () => {
+  return <Drag />;
+};
+
+export default App;
---
+++ src/pages/JellyScroll/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import styles from './styles';
+
+const JellyScroll: React.FC = () => {
+  return (
+    <View>
+      <Text>Pega poha...</Text>
+    </View>
+  );
+}
+
+export default JellyScroll;
